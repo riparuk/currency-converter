3eaea3e98d26de0175d5d9762284c3e15a60d6ed
Furqan Al Ghifari Zulva <114077972+furqanx@users.noreply.github.com>
2022-10-22 15:38:54 +0700

    Create Dollar.java

---
+++ Dollar.java
@@ -0,0 +1,17 @@
+/**
+ * Write a description of class Dollar here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Dollar extends MataUang{
+    private int x;
+
+    /**
+     * Constructor for objects of class Dollar
+     */
+    public Dollar(){
+        // initialise instance variables
+        x = 0;
+    }
+}
