7937ce59b5233564f4e6ef24cced89bc5977ef01
Furqan Al Ghifari Zulva <114077972+furqanx@users.noreply.github.com>
2022-10-22 15:37:45 +0700

    Create Rupiah.java

---
+++ Rupiah.java
@@ -0,0 +1,16 @@
+*
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Rupiah extends MataUang{
+    private int amount;
+
+    /**
+     * Constructor for objects of class Rupiah
+     */
+    public Rupiah(){
+        amount = 0;
+    }
+
+   
+}
