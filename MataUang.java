5a735e24252d319741030f3dd8622a7867343953
Furqan Al Ghifari Zulva <114077972+furqanx@users.noreply.github.com>
2022-10-22 15:23:51 +0700

    Create MataUang.java

---
+++ MataUang.java
@@ -0,0 +1,19 @@
+/**
+ * Write a description of class MataUang here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public abstract class MataUang{
+    private int amount;
+  
+    /**
+     * Constructor for objects of class MataUang
+     */
+    public MataUang()
+    {
+        // initialize instance variables
+        amount = 0;
+    }
+  
+}
