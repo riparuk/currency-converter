f79ae2a5f4e87915f0068770d9cf6b77c95654d8
Furqan Al Ghifari Zulva <114077972+furqanx@users.noreply.github.com>
2022-10-22 15:39:50 +0700

    Create Euro.java

---
+++ Euro.java
@@ -0,0 +1,17 @@
+/**
+ * Write a description of class Euro here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Euro extends MataUang{
+    private int x;
+
+    /**
+     * Constructor for objects of class Euro
+     */
+    public Euro(){
+        // initialise instance variables
+        x = 0;
+    }
+}
